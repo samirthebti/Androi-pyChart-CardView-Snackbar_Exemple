9155441fb4ff1bb3e345d1dbb95ed8fc03b46136
Thebti Samir <thebti.samir.dg@gmail.com>
2016-07-05 17:52:13 +0100

    add pychart ,  snackbar , card view component

---
+++ app/src/main/java/com/example/thesam/myapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.thesam.myapplication;
+
+import android.graphics.Color;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private PieChart pieChart;
+    private ArrayList<Entry> data;
+    private ArrayList<String> label;
+    private FloatingActionButton fab;
+    private CoordinatorLayout coordinatorLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fab = (FloatingActionButton) findViewById(R.id.add);
+        pieChart = (PieChart) findViewById(R.id.chart);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        //label initialisation
+        label = new ArrayList<>();
+        label.add("Algerie");
+        label.add("Maroc");
+        label.add("TUnisie");
+        //data initialisation
+        data = new ArrayList<>();
+        data.add(new Entry(0.2f, 0));
+        data.add(new Entry(0.2f, 1));
+        data.add(new Entry(0.50f, 2));
+        final PieDataSet dataSet = new PieDataSet(data, "data");
+        final PieData pieData = new PieData(label, dataSet);
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        pieChart.setCenterText("Pie Chart");
+        pieChart.setData(pieData);
+        pieChart.animateY(1000);
+        pieChart.setBackgroundColor(5);
+        pieData.setDrawValues(true);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snackbar = Snackbar.make(coordinatorLayout, "Adding Action", Snackbar.LENGTH_LONG)
+                        .setAction("Dismiss", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        });
+                snackbar.setActionTextColor(Color.RED);
+
+                snackbar.show();
+            }
+        });
+        pieChart.notifyDataSetChanged();
+
+    }
+}
---
+++ app/src/main/java/com/example/thesam/myapplication/oneFragment.java
@@ -0,0 +1,54 @@
+package com.example.thesam.myapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link oneFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link oneFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class oneFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public oneFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
